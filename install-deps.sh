9cf352591bd686817ddc2986e81049bc76066d64
Andrey Volkov <a.volkov@semrush.com>
2014-12-05 12:17:58 +0300

    initial commit

---
+++ install-deps.sh
@@ -0,0 +1,18 @@
+
+
+# my magic photobooth symlink -> dropbox. I love it.
+# first move Photo Booth folder out of Pictures
+# then start Photo Booth. It'll ask where to put the library.
+# put it in Dropbox/public
+
+# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
+# then you grab public URL and send off your video message in a heartbeat.
+
+
+# for the c alias (syntax highlighted cat)
+sudo easy_install Pygments
+
+
+# chrome canary as default
+# on a mac you can set chrome canary as your default inside of Safari preferences :)
+
