4a04c2783c42819e4d41e0c2ac0438896adcd150
Andrey Volkov <a.volkov@semrush.com>
2015-02-12 15:30:54 +0300

    added more symlinks to install-symlinks.sh

--- install-symlinks.sh
+++ install-symlinks.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 ln -s ~/dotfiles/Preferences.sublime-settings Preferences.sublime-settings
-
+ln -s ~/dotfiles/.aliases .aliases
+ln -s ~/dotfiles/.bash_prompt .bash_prompt
+ln -s ~/dotfiles/.bashrc .bashrc
+ln -s ~/dotfiles/.exports .exports
+ln -s ~/dotfiles/.gitattributes .gitattributes
+ln -s ~/dotfiles/.gitignore .gitignore
+ln -s ~/dotfiles/.inputrc .inputrc
